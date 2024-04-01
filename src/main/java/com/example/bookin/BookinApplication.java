6e7b946fa651ed5ff810b42d51d13450cb4db703
wodydl0 <poty9798@naver.com>
2024-04-01 17:57:16 +0900

    first commit

---
+++ src/main/java/com/example/bookin/BookinApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookin/BookinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
